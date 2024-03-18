4be97fd21ee7f190322022f382c5f9c90f66e169
ThiagoSS1 <thiago_seta@hotmail.com>
2024-03-18 20:00:36 -0300

    feat: remover livro

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ class Biblioteca {
   adicionarLivro (livro) {
     this.livros.push(livro);
   }
+
+  removerLivro (livro) {
+    this.livros = this.livros.filter(l => l !== livro);
+  }
 }
 
 class Livro {
@@ -38,3 +42,21 @@ document.getElementById('adicionar').addEventListener('click', function (event) 
   li.textContent = titulo + ' - ' + autor;
   document.getElementById('livros').appendChild(li);
 });
+
+document.getElementById('remover').addEventListener('click', function (event) {
+  event.preventDefault();
+  let titulo = document.getElementById('title').value;
+  let autor = document.getElementById('author').value;
+  let livro = new Livro(titulo, autor);
+  simulador.biblioteca.removerLivro(livro);
+
+  let ul = document.getElementById('livros');
+  let li = ul.getElementsByTagName('li');
+  for (let i = 0; i < li.length; i++) {
+    if (li[i].textContent === titulo + ' - ' + autor) {
+      ul.removeChild(li[i]);
+      break;
+    }
+  }
+
+})
